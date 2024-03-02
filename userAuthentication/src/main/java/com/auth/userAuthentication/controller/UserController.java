fe22bf2ebf67cd1952b568c0863e51b320dd1a0d
Ernest Emmanuel Utibe <em123nest@gmail.com>
2024-03-02 02:19:53 +0100

    Update UserController.java

--- userAuthentication/src/main/java/com/auth/userAuthentication/controller/UserController.java
+++ userAuthentication/src/main/java/com/auth/userAuthentication/controller/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/users")
